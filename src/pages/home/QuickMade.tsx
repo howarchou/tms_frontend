ae61e818776054445eba9aa7928648dad721c272
howarchou <howarchou@sina.com>
2022-03-31 21:40:55 +0800

    add quickMade

--- src/pages/home/QuickMade.tsx
+++ src/pages/home/QuickMade.tsx
@@ -8,23 +8,33 @@ import { genAgl } from "@/lib/fcagl"
 import './QuickMade.less'
 
 export default function () {
-  const defaultVaule = { remark: '首页banner免费咨询' }
+  const defaultValue = {
+    remark: '首页banner免费咨询',
+    contact: '',
+    contact_mobile: '',
+    captcha: ''
+  }
   const [ error, setError ] = useState('')
-  const [ values, setValues ] = useState<any>(defaultVaule)
+  const [ values, setValues ] = useState<any>(defaultValue)
   const {
     second, handleGetSmsCode, message
   }
     = useCaptcha(values)
   const handleSubmit = () => {
     if (!values?.contact) {
-      setError('请输入联系人')
+      setError('请输入联系人您的称呼')
       return
     }
     if (!values?.contact_mobile) {
       setError('请输入手机号')
       return
     }
+    if (!values?.captcha) {
+      setError('请输入验证码')
+      return
+    }
     saveOrders({ ...values }).then(() => {
+      setValues(defaultValue)
       //TODO 增加埋点
       genAgl()
     })
@@ -49,11 +59,12 @@ export default function () {
   }
   return (
     <div className='quick-wrapper'>
-      <p><span>公司团建活动策划</span>| 今日还剩7个免费团建活动方案名额</p>
+      <p><span>公司团建活动策划</span></p>
       <div className='form-item'>
         <input placeholder='请输入您的称呼'
           type='text'
-          name='name'
+          name='contact'
+          value={ values.contact }
           className='item-name'
           onChange={ e => handleInputChange('contact', e.target.value) }
           required/>
@@ -62,20 +73,21 @@ export default function () {
           type='text'
           className="item-phone"
           name="contact_mobile"
+          value={ values.contact_mobile }
           onChange={ e => handleInputChange('contact_mobile', e.target.value) }
           required/>
         <input placeholder='验证码'
           type='text'
           className="item-captcha"
           name="captcha"
+          value={ values.captcha }
           onChange={ e => handleInputChange('captcha', e.target.value) }
           required/>
         { second !== undefined && second >= 0 ? (
           <button className="sms-btn disable">已下发{ second }s</button>
         ) : (
           <button className="sms-btn"
-            onClick={ () => handleGetSmsCode() }>
-            获取验证码 </button>
+            onClick={ () => handleGetSmsCode() }>获取验证码 </button>
         ) }
 
         {
